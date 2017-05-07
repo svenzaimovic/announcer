c2c86a0bc14667c3b1ed5f94fc502d35a497a846
svenzaimovic <svenzaimovic@gmail.com>
2017-05-07 19:17:25 +0200

    CRUD handlers complete, ready for MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 app.listen(3000, function(){
 	console.log("Listening on 3000")
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+//CRUD Handlers
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+	console.log(req.body)
 })
