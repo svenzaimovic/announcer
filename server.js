5ac18446ce39b92acf286880440e051bb1b2820b
svenzaimovic <svenzaimovic@gmail.com>
2017-05-07 16:45:57 +0200

    Set up the first callback for the get

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-console.log('May the Node be with you!')
 
 app.listen(3000, function(){
 	console.log("Listening on 3000")
+})
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
 })
