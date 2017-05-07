923656143ad41e9f5dfb41d090df5bd8e34beac8
svenzaimovic <svenzaimovic@gmail.com>
2017-05-07 22:31:32 +0200

    Integrated with MongoDB and managed to push data to the server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
-const app = express();
+const MongoClient = require('mongodb').MongoClient
+const app = express()
 
-app.listen(3000, function(){
-	console.log("Listening on 3000")
+
+
+MongoClient.connect('mongodb://admin:SSSTAdmin@ds133261.mlab.com:33261/announcer', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, function(){
+		console.log("Listening on 3000")
+	})
+	db.collection('quotes').find().toArray(function(err, results) {
+		console.log(results)
+		// send HTML file populated with quotes here
+	})
 })
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -12,8 +23,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
+	var cursor = db.collection('quotes').find()
+	console.log(cursor)
 })
 
 app.post('/quotes', (req, res) => {
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('Saved to database')
+		res.redirect('/')
+	})
 	console.log(req.body)
 })
