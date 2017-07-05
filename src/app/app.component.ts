0b15ff75318e83a340a4d83764843ef1cedb9f51
svenzaimovic <svenzaimovic@gmail.com>
2017-07-05 10:42:38 +0200

    in case I mess up... revert to here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Test app for the announcer using the dummy data';
+export class AppComponent implements OnInit {
+
+  constructor(private usersService: UsersService) { }
+
+isLoggedIn = false
+
+  ngOnInit() {
+
+    function me(){
+      this.usersService.getMe().subscribe(user => {
+        console.log('Got me', user);
+        this.isLoggedIn = true
+        console.log(this.isLoggedIn)
+      })
+    }  
+  }
+
+
+  title = 'SSST Announcer';
 }
